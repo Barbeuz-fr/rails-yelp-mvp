fd2fc892da20b17765fb6a514ba1fa12c27a5820
Laurent Barbezieux <laurent.barbezieux@gmail.com>
2020-02-12 15:13:09 +0100

    rake ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+(1..10).to_a.each_with_index { |element, index|
+  restaurant = Restaurant.new(name: "resto",
+                address: "Obkerkamp",
+                category: "italian",
+                phone_number: index)
+  restaurant.save
+  (1..3).to_a.each_with_index { |element2, index2|
+    review = Review.new(rating: index2, restaurant_id: index)
+    review.save
+  }
+}
